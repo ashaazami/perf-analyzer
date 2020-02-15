2f36fb2e9150a0bf51ec5fb553fc9a2af6ca0cd5
Ashkan Aazami <aazami@gmail.com>
2020-02-15 10:33:08 -0800

    adding performace logger and visualizer

---
+++ perf_loger/example.py
@@ -0,0 +1,15 @@
+import time
+
+from log_performance import log_performance
+
+
+@log_performance
+def sleep_2_seconds():
+    time.sleep(2)
+    return
+
+
+if __name__ == '__main__':
+    sleep_2_seconds()
+    time.sleep(1)
+    sleep_2_seconds()
